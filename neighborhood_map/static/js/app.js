5f583d177982ef5f01b18dfbd2e67bcdaad1ce56
Hyungmo Gu <guhyungm7@gmail.com>
2017-04-01 16:04:15 -0700

    refactor: Enabling 'App' to interact with 'InfoWindow' and 'GMap'
    Summary:
      - GMap and the List-View DOM are now stared in 'App' object
      - App contains instance variables containing an instance of
        GMap and the List-view DOM
      - Made sure the application follows the layered pattern
    
    Motivation:
      - Done to satisfy Udacity's requirement on App architecture:
        "Knockout should not be used to handle Google Map API "
      - Done to prepare for more complex features involving markers
        from Google maps and the list-view/info-window

--- neighborhood_map/static/js/app.js
+++ neighborhood_map/static/js/app.js
@@ -1,10 +1,3 @@
-var Map = function() {
-	var self = this;
-
-	self.map = new google.maps.Map(document.getElementById('g-map'),{zoom: 5,center: {'lat':49.226967,'lng':-122.948692}});
-
-};
-
 var Event = function(event) {
 	var self = this;
 	self.name = event['name']['text'];
@@ -15,41 +8,104 @@ var Event = function(event) {
 	self.url = event['url'];
 };
 
+var Model = {
+	data: [],
+	addData: function(data) {
+		$.map(data, function(item){
+			Model.data.push(new Event(item));
+		});
+	},
+	refreshData: function(data) {
+		Model.data = [];
+		Model.addData(data);
+	}
+};
+
+var GMap = function(){
+	var self = this;
+
+	self.init = function() {
+		self.map = new google.maps.Map(document.getElementById('g-map'),{zoom: 5,center: {'lat':49.226967,'lng':-122.948692}});
+	};
+};
+
 var App = {
-	load: function() {
+	init: function() {
+		var self = this;
+
+		self.infoWindow = new InfoWindow();
+		self.gMap = new GMap();
+
+		App._load();
+	},
+	_load: function(callback) {
+		var self = this;
+		ko.applyBindings(self.infoWindow);
 		$.ajax({
 			url: 'https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.within=20km&location.latitude=49.226967&location.longitude=-122.948692&date_modified.keyword=this_week&expand=organizer,venue&token=SOLRRNOSEG4UHYXOXLNG',
 			type: 'GET',
+			timeout: 5000,
 			success: function(result, status) {
-				Model.data = result['events'];
-				ko.applyBindings(new InfoWindow());
+
+				if (result['pagination']['object_count'] == 0) {
+					self.infoWindow.displayError('not_found');
+					return;
+				};
+
+				Model.addData(result['events']);
+
+				self.gMap.init();
+				self.infoWindow.init();
 			},
-			error: function(error) {
+			error: function(xhr, status, error) {
 				console.log('Error occured while loading events: ' + error);
+
+				if (status == 'error') {
+					self.infoWindow.displayError('default');
+					return;
+				};
+				if (status == 'timeout') {
+					self.infoWindow.displayError('timeout');
+					return;
+				};
 			}
 		});
 	},
 	search: function(keyword, callback) {
+		var self = this;
+
 		$.ajax({
 			url: 'https://www.eventbriteapi.com/v3/events/search/?q=' + keyword + '&sort_by=distance&location.within=20km&location.latitude=49.226967&location.longitude=-122.948692&date_modified.keyword=this_week&expand=organizer,venue&token=SOLRRNOSEG4UHYXOXLNG',
 			type: 'GET',
 			timeout: 5000,
 			success: function(result, status) {
-				Model.data = result['events'];
-				callback(result, status);
+
+				if (result['pagination']['object_count'] == 0) {
+					self.infoWindow.displayError('not_found');
+					return;
+				};
+
+				Model.refreshData(result['events']);
+
+				self.infoWindow.updateEvents();
+
+				self.infoWindow.goBackToEventList();
 			},
 			error: function(xhr, status, error) {
 				console.log('Error occured while searching events: ' + error);
-				callback(error, status);
+
+				if (status == 'error') {
+					self.infoWindow.displayError('default');
+					return;
+				};
+				if (status == 'timeout') {
+					self.infoWindow.displayError('timeout');
+					return;
+				};
 			}
 		});
 	}
 };
-
-var Model = {
-	data: []
-};
-
 
 var InfoWindow = function() {
 	var self = this;
@@ -79,7 +135,7 @@ var InfoWindow = function() {
 	self.init = function() {
 		// Display list of events
 		$.map(Model.data, function(event){
-			self.events.push(new Event(event));
+			self.events.push(event);
 			self.showEventList(true);
 			self.showEventDescription(false);
 		});
@@ -87,37 +143,23 @@ var InfoWindow = function() {
 
 	self.searchEvents = function() {
 		var sanitizedKeywords = encodeURIComponent(self.searchKeywords()).replace(/%20/g, '+');
-		App.search(sanitizedKeywords, function(result, status) {
-
-			if (status == 'error') {
-				self.displayError('default');
-				return;
-			};
-			if (status == 'timeout') {
-				self.displayError('timeout');
-				return;
-			};
-			if (result['pagination']['object_count'] == 0) {
-				self.displayError('not_found');
-				return;
-			};
-
-			// Refresh the observable array
-			self.events([]);
-
-			// Update the event list
-			$.map(Model.data, function(eventItem) {
-				self.events.push(new Event(eventItem));
-			});
-
-			self.goBackToEventList();
-		});
+		App.search(sanitizedKeywords);
 	};
 
 	self.loadDescription = function(eventItem) {
 		self.event(eventItem);
 		self.showEventList(false);
 		self.showEventDescription(true);
+	};
+
+	self.updateEvents = function() {
+		// Refresh the observable array
+		self.events([]);
+
+		// Update the event list
+		$.map(Model.data, function(eventItem) {
+			self.events.push(eventItem);
+		});
 	};
 
 	self.goBackToEventList = function() {
@@ -127,7 +169,6 @@ var InfoWindow = function() {
 	};
 
 	self.displayError = function(type) {
-
 		// Determine error display type
 		if (type == 'default') {
 			self.showDefaultError(true);
@@ -149,7 +190,6 @@ var InfoWindow = function() {
 		self.showEventDescription(false);
 	}
 
-	self.init();
 };
 
-App.load();
+App.init();
